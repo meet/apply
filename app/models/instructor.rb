0ce7aa9bfafe698aed96270e21a804047d3a2910
Eric Timmons <etimmons@mit.edu>
2013-10-15 11:18:18 -0400

    Adding option for no MIT affiliation.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -4,7 +4,7 @@ class Instructor < ActiveRecord::Base
   include Application
   
   validates_presence_of :first_name, :last_name, :major
-  validates_inclusion_of :status, :in => [ 'Undergraduate', 'Grad student', 'Alumnus/a' ]
+  validates_inclusion_of :status, :in => [ 'Undergraduate', 'Grad student', 'Alumnus/a', 'None' ]
   validates_presence_of :graduation_year, :unless => "status == 'Grad student'"
   validates_presence_of :why_meet
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
